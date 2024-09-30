d96cf4be38f2f7bce0cd37ea46bae7e72456551f
ilan elhayani <ilan.elhayani@gmail.com>
2024-09-30 12:24:06 +0300

    added responsive support to footer.tsx

--- frontend/nextjs/components/Footer.tsx
+++ frontend/nextjs/components/Footer.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 import Link from 'next/link';
-//import Modal from './Settings/Modal';
 import Modal from '@/components/Settings/Modal';
 import { FC } from 'react';
 
@@ -30,17 +29,21 @@ interface FooterProps {
 
 const Footer: FC<FooterProps> = ({ setChatBoxSettings, chatBoxSettings }) => {
   return (
-    <div className="container flex min-h-[72px] items-center justify-between border-t border-[#D2D2D2] px-4 pb-3 pt-5 lg:min-h-[72px] lg:px-0 lg:py-5">
-      <Modal setChatBoxSettings={setChatBoxSettings} chatBoxSettings={chatBoxSettings} />
-      <div className="text-sm text-gray-500">
-        © {new Date().getFullYear()}השרת של אילן - עמית מחקר אוטונומי
+    <footer className="border-t border-[#D2D2D2] bg-gradient-to-r from-[#151A2D] to-[#111827] text-white">
+      <div className="container mx-auto px-4 py-3 sm:px-6 sm:py-5 lg:px-8">
+        <div className="flex flex-col items-center justify-between gap-4 sm:flex-row">
+          <Modal setChatBoxSettings={setChatBoxSettings} chatBoxSettings={chatBoxSettings} />
+          <div className="text-center text-xs sm:text-sm text-gray-300">
+            © {new Date().getFullYear()} השרת של אילן - עמית מחקר אוטונומי
+          </div>
+          <div className="flex items-center justify-center">
+            <Link href="https://www.ilanel.co.il" target="_blank" className="transition-transform hover:scale-110">
+              <Image src="/img/github.svg" alt="github" width={24} height={24} className="sm:w-[30px] sm:h-[30px]" />
+            </Link>
+          </div>
+        </div>
       </div>
-      <div className="flex items-center gap-3">
-        <Link href="https://www.ilanel.co.il" target="_blank">
-          <Image src="/img/github.svg" alt="github" width={30} height={30} />
-        </Link>
-      </div>
-    </div>
+    </footer>
   );
 };
 
